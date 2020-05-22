84f6806486a23ecff2852dd3f0b8743a6da96332
youngrake <youngrake@protonmail.com>
2020-05-22 14:45:35 +0300

    fixed restart, inputhandler

--- src/main/java/game/InputHandler.java
+++ src/main/java/game/InputHandler.java
@@ -9,18 +9,19 @@ import actors.Player;
  * @author ghast
  *
  */
-public class InputHandler {
+public class InputHandler extends Thread {
 
 	private Invaders invaders = null;
 	private Player player  = null;
 	public Action action;
+	public KeyEvent event;
 
 	public InputHandler(Invaders invaders, Player player) {
 		this.invaders = invaders;
 		this.player = player;
 	}
 
-	public void handleInput(KeyEvent event) {
+	public void run() {
 		if (action == Action.PRESS) {
 			if (KeyEvent.VK_ENTER == event.getKeyCode()) {
 				if (invaders.gameOver || invaders.gameWon) {
